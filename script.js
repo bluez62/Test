53106897e9c0762c7ddd46318b19096ef25d6796
bluez62 <nicholaspaulrodriguez13@gmail.com>
2026-09-22 11:14:31 -0500

    update

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ let items = [
     { name: "Faith Lazaneo", points: 68065, dailyGrowth: 28 },
     { name: "Jorge Gasca", points: 64385, dailyGrowth: 20 },
     { name: "Isabella Gasca", points: 64337, dailyGrowth: 26 },
-    { name: "Jerry Parker", points: 62492, dailyGrowth: 20 },
+    { name: "Jerry Parker", points: 62492, dailyGrowth: 23 },
     { name: "Josiah Parker", points: 62192, dailyGrowth: 20 },
     { name: "Josiah Lazaneo", points: 61795, dailyGrowth: 15 },
     { name: "Ryan Brandt", points: 59617, dailyGrowth: 18 },
